124521c564997c6e31d2571bb6d99f4916f0a36d
João Paulo <jpresendejava@gmail.com>
2024-06-14 10:15:25 -0400

    subindo o grafico de pizza

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+/////////grafico de barras////////////////
 class BarChart extends StatelessWidget {
   final List<double> data;
   final List<Color> colors;
@@ -10,7 +14,7 @@ class BarChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      size: Size(450, 300), 
+      size: Size(450, 300),
       painter: BarChartPainter(data: data, colors: colors, labels: labels),
     );
   }
@@ -20,15 +24,17 @@ class BarChartPainter extends CustomPainter {
   final List<double> data;
   final List<Color> colors;
   final List<String> labels;
-  final double barSpacing = 4; 
+  final double barSpacing = 4;
 
-  BarChartPainter({required this.data, required this.colors, required this.labels});
+  BarChartPainter(
+      {required this.data, required this.colors, required this.labels});
 
   @override
   void paint(Canvas canvas, Size size) {
     final double totalBarWidth = size.width - (barSpacing * (data.length + 2));
     final double barWidth = totalBarWidth / data.length;
-    final double maxData = data.reduce((value, element) => value > element ? value : element);
+    final double maxData =
+        data.reduce((value, element) => value > element ? value : element);
     final double unitHeight = size.height / maxData;
 
     for (int i = 0; i < data.length; i++) {
@@ -44,14 +50,67 @@ class BarChartPainter extends CustomPainter {
 
       final TextPainter textPainter = TextPainter(
         text: TextSpan(
-          text: 'R\$${data[i].toStringAsFixed(0)}', 
-          style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),
+          text: 'R\$${data[i].toStringAsFixed(0)}',
+          style: TextStyle(
+              color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),
         ),
         textAlign: TextAlign.center,
         textDirection: TextDirection.ltr,
       );
       textPainter.layout(minWidth: 0, maxWidth: barWidth);
-      textPainter.paint(canvas, Offset(x + (barWidth / 2 - textPainter.width / 2), y - 20)); 
+      textPainter.paint(
+          canvas, Offset(x + (barWidth / 2 - textPainter.width / 2), y - 20));
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+///////////fim grafico de barras////////////////
+
+class PieChart extends StatelessWidget {
+  final List<double> data;
+  final List<Color> colors;
+
+  PieChart({required this.data, required this.colors});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: Size(400, 400), // Tamanho do gráfico de pizza
+      painter: PieChartPainter(data: data, colors: colors),
+    );
+  }
+}
+
+class PieChartPainter extends CustomPainter {
+  final List<double> data;
+  final List<Color> colors;
+
+  PieChartPainter({required this.data, required this.colors});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    double total = data.reduce((acc, value) => acc + value);
+    double startAngle = 0;
+    double centerX = size.width / 2;
+    double centerY = size.height / 2;
+    double radius = min(size.width / 2, size.height / 2);
+
+    for (int i = 0; i < data.length; i++) {
+      double sweepAngle = 2 * pi * (data[i] / total);
+      final paint = Paint()
+        ..color = colors[i % colors.length]
+        ..style = PaintingStyle.fill;
+      canvas.drawArc(
+          Rect.fromCircle(center: Offset(centerX, centerY), radius: radius),
+          startAngle,
+          sweepAngle,
+          true,
+          paint);
+      startAngle += sweepAngle;
     }
   }
 
@@ -69,9 +128,6 @@ void main() {
       ),
       body: Column(
         children: [
-
-            
-
           Container(
             height: 300,
             alignment: Alignment.center,
@@ -83,8 +139,32 @@ void main() {
             margin: EdgeInsets.all(10),
             child: BarChart(
               data: [100, 200, 150, 500, 700],
-              colors: [Color(0xff07c2f3), Color(0xffe30224), Color.fromARGB(255, 212, 158, 57), Color.fromARGB(255, 5, 143, 74)],
+              colors: [
+                Color(0xff07c2f3),
+                Color(0xffe30224),
+                Color.fromARGB(255, 212, 158, 57),
+                Color.fromARGB(255, 5, 143, 74)
+              ],
               labels: ['A', 'B', 'C', 'D', 'E'],
+            ),
+          ),
+          Container(
+            height: 300,
+             decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: Color.fromARGB(255, 233, 233, 233),
+            ),
+            padding: EdgeInsets.only(top: 30, left: 4),
+            margin: EdgeInsets.all(10),
+            child: PieChart(
+              data: [30, 20, 25, 15, 10],
+              colors: [
+                 Color(0xff07c2f3),
+                Color(0xffe30224),
+                Color.fromARGB(255, 212, 158, 57),
+                Color.fromARGB(255, 5, 143, 74),
+                Colors.orange
+              ],
             ),
           ),
         ],
